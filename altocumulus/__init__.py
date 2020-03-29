8c6dda5d82fc0184610dc01d80c6c11205ff1b1c
muralweaver <almphaphuli@tuta.io>
2020-03-29 17:52:34 +0200

    Create login functionality and include Flask-Login attributes to User model

--- altocumulus/__init__.py
+++ altocumulus/__init__.py
@@ -2,6 +2,7 @@
 from flask import Flask, render_template
 # from sentry_sdk.integrations.flask import FlaskIntegration
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 # To configure the SDK, initialize it with the integration before or after your app has been initialized:
 # sentry_sdk.init(
 #     dsn="https://0d00b25a81934bbfb047905ded8997d2@sentry.io/5170498",
@@ -15,6 +16,15 @@ def create_app():
     app.config['SECRET_KEY'] = '47LWxND4o83j4K4iuopO'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
     db.init_app(app)
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+    from .models import User
+    
+    @login_manager.user_loader
+    def load_user(user_id):
+        # since the user_id is just the primary key of our user table, use it in the query for the user
+        return User.query.get(int(user_id))
     
     from altocumulus.main import main as main_blueprint
     app.register_blueprint(main_blueprint)
--- altocumulus/main.py
+++ altocumulus/main.py
@@ -1,4 +1,6 @@
 from flask import Blueprint, render_template
+from flask_login import login_required, current_user
+from . import db
 
 main = Blueprint('main', __name__)
 
@@ -7,5 +9,6 @@ def index():
     return render_template('index.html')
 
 @main.route('/profile')
+@login_required
 def profile():
-    return 'Profile'
+    return render_template('profile.html', name=current_user.name)
