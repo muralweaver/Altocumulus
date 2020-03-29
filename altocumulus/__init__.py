68846d6a697c136150bdbbc745bf2ffe4a596c2b
muralweaver <almphaphuli@tuta.io>
2020-03-29 17:10:58 +0200

    Create sign up method

--- altocumulus/__init__.py
+++ altocumulus/__init__.py
@@ -1,7 +1,7 @@
 # import sentry_sdk
 from flask import Flask, render_template
 # from sentry_sdk.integrations.flask import FlaskIntegration
-
+from flask_sqlalchemy import SQLAlchemy
 # To configure the SDK, initialize it with the integration before or after your app has been initialized:
 # sentry_sdk.init(
 #     dsn="https://0d00b25a81934bbfb047905ded8997d2@sentry.io/5170498",
@@ -9,15 +9,21 @@ from flask import Flask, render_template
 #     sample_rate=0.4,
 #     max_breadcrumbs=25
 # )
-
+db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
+    app.config['SECRET_KEY'] = '47LWxND4o83j4K4iuopO'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+    db.init_app(app)
     
     from altocumulus.main import main as main_blueprint
     app.register_blueprint(main_blueprint)
     
     from altocumulus.about import about as about_blueprint
     app.register_blueprint(about_blueprint)
+    
+    from altocumulus.auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
     
     from altocumulus.graphs import graphs as graphs_blueprint
     app.register_blueprint(graphs_blueprint)
--- altocumulus/auth.py
+++ altocumulus/auth.py
@@ -1,15 +1,37 @@
-from flask import Blueprint
+import requests
+from flask import Blueprint, render_template, url_for, request, redirect, flash
+from werkzeug.security import generate_password_hash, check_password_hash
+from .models import User
 from . import db
-
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login')
 def login():
-    return 'Login'
+    return render_template('login.html')
+
 
 @auth.route('/signup')
 def signup():
-    return 'Signup'
+    # code to validate and add user to database goes here
+    return render_template('signup.html')
+
+
+@auth.route('/signup', methods=['POST'])
+def signup_post():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    
+    user = User.query.filter_by(email=email).first()
+    if user:
+        flash('Email address already exists')
+        return redirect(url_for('auth.signup'))
+    # create new user with the form data. Hash the password so plaintext version isn't saved.
+    new_user = User(email=email, password=generate_password_hash(password, method='sha256'))
+    # add the new user to the database
+    db.session.add(new_user)
+    db.session.commit()
+    return redirect(url_for('auth.login'))
+
 
 @auth.route('/logout')
 def logout():
