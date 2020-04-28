a35ffee74549a418f8bfffd7c77c5d2d91e3040d
muralweaver <awi.mphaphuli@tuta.io>
2020-04-28 20:45:30 +0200

    Add movie suggest endpoint and blueprint

--- altocumulus/movie_suggest.py
+++ altocumulus/movie_suggest.py
@@ -1,16 +1,24 @@
-from flask import Blueprint, render_template
-from flask_login import login_required, current_user
-from . import db
+from flask import Blueprint
+import requests
 
-main = Blueprint('main', __name__)
-
-
-@main.route('/')
-def index():
-    return render_template('index.html')
+movie_suggest = Blueprint('movie_suggest', __name__)
 
 
-@main.route('/profile')
-@login_required
-def profile():
-    return render_template('profile.html', name=current_user.name)
+@movie_suggest.route('/api/movie')
+def suggest_movie(movie):
+    baseurl = "https://tastedive.com/api/similar"
+    params_diction = {"q": movie, "type": 'movies', "limit": 5}
+    resp = requests.get(baseurl, params=params_diction)
+    word_ds = resp.json()
+    return word_ds
+
+
+def extract_movie_titles(titles):
+    makelist = []
+    for k, v in titles.items():
+        for x in v['Results']:
+            makelist.append(x['Name'])
+        return makelist
+
+
+print(extract_movie_titles(suggest_movie("A movie")))
